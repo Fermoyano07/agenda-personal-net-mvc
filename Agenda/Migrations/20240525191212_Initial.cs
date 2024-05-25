cceac200e9bf7bb251e50d5727ba53c3a691f218
Usuario <Usuario@DESKTOP-34FFSVL>
2024-05-25 19:53:33 -0300

    Agregar archivos de proyecto.

---
+++ Agenda/Migrations/20240525191212_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agenda.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacto", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacto");
+        }
+    }
+}
---
+++ Agenda/Models/Contacto.cs
@@ -0,0 +1,11 @@
+﻿namespace Agenda.Models
+{
+    public class Contacto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set;}
+        public string Apellido { get; set;}
+        public int Numero { get; set;}
+        public string Email { get; set;}
+    }
+}
